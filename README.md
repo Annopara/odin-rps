772518c19f5151f150a64389e87c71b01a1cb188
Ann Opara <oparaannie51@gmail.com>
2024-08-31 01:52:28 -0700

    adds functtion to get computer choice

---
+++ README.md
@@ -0,0 +1,14 @@
+# ODIN Rock-Paper-Scissors
+
+## Project Overview
+
+This project features a simple game of rock-paper-scissor between a computer bot and user.
+
+### Features
+
+- functional game played in GUI
+
+## Project Structure
+
+- `index.html`: The main HTML file containing the structure of the landing page.
+- `script.js`: The js file where the game functions are implemented.
--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const greetings = "Hi, and welcome";
-console.log(greetings);
+// Get random numbers
+function getRandom() {
+  return Math.random();
+}
+
+const getComputerChoice = () => {
+  const randomChoice = getRandom();
+  console.log("Random: ", randomChoice);
+  if (randomChoice <= 0.33) {
+    console.log("Rock");
+  } else if (randomChoice <= 0.66) {
+    console.log("Paper");
+  } else {
+    console.log("Scissors");
+  }
+};
+
+getComputerChoice(0.44);
